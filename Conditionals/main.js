9deb7083b916eaf7ed9fa396d40ea8f5b6a6da21
Delgaa <112789365+Delgaa@users.noreply.github.com>
2023-12-26 17:37:36 -0300

    Condicionales

--- Conditionals/main.js
+++ Conditionals/main.js
@@ -15,14 +15,14 @@ if ( num1 > num2 ) {
 }else if( num1 < num2 ){
     console.log(num2)
 }else{
-    console.log("Los numeros son iguales")
+    console.log("The numbers are equal")
 }
 
 // Punto 3
 if( num1 == num2 ){
-    console.log("Los numeros son iguales")
+    console.log("The numbers are equal")
 }else{
-    console.log("Los números son diferentes")
+    console.log("The numbers are different")
 }
 
 // Punto 4
@@ -30,11 +30,11 @@ let date1 = "2018-9-2"
 let date2 = "2023-7-5"
 
 if ( date1 > date2 ) {
-    console.log(`La fecha ${date1} es mayor que ${date2}`)
+    console.log(`The date ${date1} is greater than ${date2}`)
 }else if (date1 == date2) {
-    console.log("Las fechas son iguales")
+    console.log("The dates are the same")
 }else{
-    console.log(`La fecha ${date2} es mayor que ${date1}`)
+    console.log(`The date ${date2} is greater than ${date1}`)
 }
 
 // Punto 5
@@ -42,11 +42,11 @@ date1 = "2010-5-17"
 date2 = "2006-12-30"
 
 if ( date1 > date2 ) {
-    console.log(`La fecha ${date1} es mayor que ${date2}`)
+    console.log(`The date ${date1} is greater than ${date2}`)
 }else if (date1 == date2) {
-    console.log("Las fechas son iguales")
+    console.log("The dates are the same")
 }else{
-    console.log(`La fecha ${date2} es mayor que ${date1}`)
+    console.log(`The date ${date2} is greater than ${date1}`)
 }
 
 // Punto 6
@@ -55,57 +55,57 @@ let number2 = 7
 let number3 = 9
 
 if ( number1 > number2 && number1 > number3) {
-    console.log(`El número ${number1} es el más grande`)
+    console.log(`The number ${number1} is the largest`)
 }else if (number2 > number1 && number2 > number3) {
-    console.log(`El número ${number2} es el más grande`)
+    console.log(`The number ${number2} is the largest`)
 } else {
-    console.log(`El número ${number3} es el más grande`)
+    console.log(`The number ${number3} is the largest`)
 }
 
 // Nivel Medio
 // Punto 1
-let color = prompt("Elige un color entre rojo verde o azul")
+let color = prompt("Choose a color between red, green or blue:")
 switch (color) {
-    case "azul":
-        console.log("El color del mar")
+    case "blue":
+        console.log("The color of the sea.")
         break;
-    case "rojo":
-        console.log("El color de la pasion")
+    case "red":
+        console.log("The color of passion")
         break;
-    case "verde":
-        console.log("El color de la naturaleza")
+    case "greed":
+        console.log("The color of nature")
         break;
     default:
-        console.log("Color ingresado incorrecto")
+        console.log("Incorrect color entered")
         break;
 }
 
 // Punto 2
-const numb1 = Number(prompt("Ingrese el primer numero del '1 al 100'."))
-const numb2 = Number(prompt("Ingrese el segundo numero del '1 al 100'."))
-const operation = prompt( "Escriba una operación a realizar(sin mayúsculas): suma, resta, división, multiplicación" )
+const numb1 = Number(prompt("Enter the first number from '1 to 100'."))
+const numb2 = Number(prompt("Enter the second number from '1 to 100'."))
+const operation = prompt( "Write an operation to perform (without capital letters): addition, subtraction, division, multiplication:" )
 
 
 if (( numb1 >= 1 && numb2 >= 1 )&&( numb1 <= 100 && numb2 <= 100 )){
     switch (operation) {
-        case "suma":
-            console.log(`El resultado de la suma de ${numb1} y ${numb2} es: ${numb1 + numb2}`)
+        case "addition":
+            console.log(`The result of the sum of ${numb1} and ${numb2} is: ${numb1 + numb2}`)
             break;
-        case "resta":
-            console.log(`El resultado de la resta de ${numb1} y ${numb2} es: ${numb1 - numb2}`)
+        case "subtraction":
+            console.log(`The result of the subtraction of ${numb1} and ${numb2} is: ${numb1 - numb2}`)
             break
-        case "multiplicacion":
-            console.log(`El resultado de la multiplicación de ${numb1} y ${numb2} es: ${numb1 * numb2}`)
+        case "multiplication":
+            console.log(`The result of the multiplying of ${numb1} and ${numb2} is: ${numb1 * numb2}`)
             break
         case "division":
-            console.log(`El resultado de la división de ${numb1} y ${numb2} es: ${numb1 / numb2}`)
+            console.log(`The result of the division of ${numb1} and ${numb2} is: ${numb1 / numb2}`)
             break
         default:
-            console.log("Operación invalida intente de nuevo")
+            console.log("Invalid opertation try again")
             break;
     }
 }else{
-    console.log("Los valores ingresados superan o son menores al propuesto")
+    console.log("The values ​​entered exceed or are less than the proposed one")
 }
 
 // Punto 3
@@ -120,63 +120,63 @@ let person2 ={
     height: 1.80
 }
 if (person1.height > person2.height && person1.age > person2.age) {
-    console.log( `${person1.name} es mas alto y mayor que ${person2.name}` )
+    console.log( `${person1.name} is taller and older than ${person2.name}` )
 }else if (person1.height < person2.height && person1.age < person2.age) {
-    console.log( `${person2.name} es mas alto y mayor que ${person1.name}` )
+    console.log( `${person2.name} is taller and older than ${person1.name}` )
 } else if ((person1.height > person2.height && person1.age < person2.age)) {
-    console.log( `${person1.name} es mas alto que ${person2.name} pero ${person2.name} es mas grande` )
+    console.log( `${person1.name} it is higher than ${person2.name} but ${person2.name} it is bigger` )
 } else {
-    console.log( `${person2.name} es mas alto que ${person1.name} pero ${person1.name} es mas grande` )
+    console.log( `${person2.name} it is higher than ${person1.name} but ${person1.name} it is bigger` )
 }
 
 // Punto 4
-let name = prompt("Ingrese su nombre completo")
-let age = Number(prompt("Ingrese su edad"))
-let height = Number(prompt("Ingrese su altura en centimetros"))
-let vision = prompt("Ingrese su vision")
+let name = prompt("Enter your full name")
+let age = Number(prompt("Enter your age"))
+let height = Number(prompt("Enter your height in centimeters"))
+let vision = prompt("Enter your vision quality")
 
 if (age >= 18 && height > 110 && vision >= 8) {
-    console.log("Estás calificado para conducir ")
+    console.log("Are you qualified to drive")
 } else {
-    console.log("No estás calificado para conducir ")
+    console.log("You are not qualified to drive")
 }
 
 // Punto 5
-const age_tip = Number(prompt("Ingrese su edad")) 
+const age_tip = Number(prompt("Enter your age")) 
 
 if (age_tip >= 0 && age_tip <= 12) {
-    alert("Infantil")
+    alert("Infant")
 } else if (age_tip >= 13 && age_tip <= 18){
-    alert("Adolescente")
+    alert("Adolescent")
 }else if (age_tip >= 19 && age_tip <= 45) {
-    alert("Joven Mayor")
+    alert("Olden Young")
 } else if (age_tip >= 45 && age_tip <= 100) {
-    alert("Personas Mayores")
+    alert("Elderly")
 } else {
-    alert("¿Es realmente tan viejo?")
+    alert("Is he really that old?")
 }
 
 // Punto 6
-const number_tip = Number(prompt("Ingrese un numero del 1 al 3."))
+const number_tip = Number(prompt("Enter a number from '1 to 3'."))
 
 switch (number_tip) {
     case 1:
-        alert(`El numero ingresado es ${number_tip}`)
-        alert(`El doble del numero ingresado es ${2 * number_tip}`)
-        alert(`Tres veces el numero ingresado es ${3 * number_tip}`)
+        alert(`The number entered is ${number_tip}`)
+        alert(`Twice the number entered is ${2 * number_tip}`)
+        alert(`Three times the number entered is ${3 * number_tip}`)
         break;
     case 2:
-        alert(`El numero ingresado es ${number_tip}`)
-        alert(`El doble del numero ingresado es ${2 * number_tip}`)
-        alert(`Tres veces el numero ingresado es ${3 * number_tip}`)
+        alert(`The number entered is ${number_tip}`)
+        alert(`Twice the number entered is ${2 * number_tip}`)
+        alert(`Three times the number entered is ${3 * number_tip}`)
         break;
     case 3:
-        alert(`El numero ingresado es ${number_tip}`)
-        alert(`El doble del numero ingresado es ${2 * number_tip}`)
-        alert(`Tres veces el numero ingresado es ${3 * number_tip}`)
+        alert(`The number entered is ${number_tip}`)
+        alert(`Twice the number entered is ${2 * number_tip}`)
+        alert(`Three times the number entered is ${3 * number_tip}`)
         break;
     default:
-        alert("Ese valor no está permitido")
+        alert("That value is not allowed")
         break;
 }
 
@@ -185,34 +185,34 @@ switch (number_tip) {
 let ticket
 let pass
 let money 
-const name_customer = prompt("Ingrese su nombre completo")
+const name_customer = prompt("Enter your full name")
 
 if (name_customer == "nombre") {
-    alert(`Bienvenido ${name_customer}`)
+    alert(`Welcome ${name_customer}`)
 } else {
-    pass = confirm("Posee usted un pase 'vip' o 'normal'?")
+    pass = confirm("Do you have a 'vip' or 'normal' pass?")
     if (pass) {
-        alert("Bienvenido, usted tiene el pase 'vip'. ")
+        alert("Welcome, you have a 'vip' pass. ")
     } else {
-        ticket = confirm("Tiene usted entrada?")
+        ticket = confirm("Do you have ticket?")
         if (ticket) {
-        ticket = confirm("Desea usar su Entrada?")
+        ticket = confirm("You want to use your ticket?")
             if(ticket){
-                alert("Bienvenido, usted a usado su entrada.")
+                alert("Welcome, you have used your ticket.")
             }else{
-                alert("Hasta luego, vuelva pronto.")
+                alert("Goodbye, come back soon.")
             }
         } else {
-            ticket = confirm("¿Quiere comprar?")
+            ticket = confirm("Do you want to buy?")
             if (ticket) {
-                money = Number(prompt("Cuanto es su dinero disponible"))
+                money = Number(prompt("How much is your available money?"))
                 if (money > 1000) {
-                    alert("Bienvenido, la compra se ha realizado exitosamente")
+                    alert("Welcome, he purchase was completed successfully.")
                 } else {
-                    alert("La compra no se ha podido realizar")
+                    alert("The purchase could not be made")
                 }
             } else {
-                alert("Hasta luego, vuelva pronto.")
+                alert("Goodbye, come back soon.")
             }
         }
     }
